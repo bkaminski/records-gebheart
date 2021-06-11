6dcee0a74ba261d47a640566133b35beb0f3e316
Ben Kaminski <ben@benkaminski.com>
2021-06-11 18:24:14 -0400

    removed cannonical from header

--- header.php
+++ header.php
@@ -4,7 +4,6 @@
 <?php get_template_part( 'parts/page', 'header-tracking' ); ?>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<link rel="canonical" href="<?php echo get_home_url(); ?>"/>
 <link rel="alternate" hreflang="x-default" href="<?php echo get_home_url(); ?>"/>
 <link rel="alternate" hreflang="en-us" href="<?php echo get_home_url(); ?>"/>
 <?php get_template_part( 'parts/page', 'favicons' ); ?>
